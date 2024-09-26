67bd2e0ea8d0e7ff3f3f450ae9044eb8fdb61c33
pibbling <pibbling@example.com>
2024-09-26 06:37:54 +0100

    maim

--- dwm/config.h
+++ dwm/config.h
@@ -54,7 +54,7 @@ static const Layout layouts[] = {
 #define MODKEY Mod1Mask
 #define SUPER  Mod4Mask
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+      	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
@@ -66,7 +66,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *lockcmd[]  = { "slock", NULL};
+static const char *lockcmd[]  = { "slock", NULL };
+static const char *shotcmd[]  = { "shot", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -104,6 +105,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ SUPER,			XK_l,	   spawn,	   {.v = lockcmd } },
+        { SUPER,                        XK_s,      spawn,          {.v = shotcmd } },
 };
 
 /* button definitions */
