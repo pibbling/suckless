5e3ac84c8da37bf779da0105780ea24dca0ac19d
pibbling <pibbling@example.com>
2024-10-01 16:49:12 +0100

    font size

--- dwm/config.h
+++ dwm/config.h
@@ -11,8 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack:size=8" };
-static const char dmenufont[]       = "Hack:size=8";
+static const char *fonts[]          = { "Hack:size=10"};
+static const char dmenufont[]       = "Hack:size=10";
 static const char col_gray1[]       = "#101010";
 static const char col_gray2[]       = "#252525";
 static const char col_gray3[]       = "#999999";
